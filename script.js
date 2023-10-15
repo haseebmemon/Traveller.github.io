8120d0b34b912fc433bb69a84074dba3d23a98f3
Abdul Haseeb Hassan <134266899+haseebmemon@users.noreply.github.com>
2023-10-15 22:28:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// navbar toggle
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function (){
+    this.classList.toggle("active");
+    header.classList.toggle("active")
+})
+
+// Showing Go top btn
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function (){
+    window.scrollY >=500 ? goTopBtn.classList.add("active"): goTopBtn.classList.remove("active");
+})
